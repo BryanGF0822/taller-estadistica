c7a196a8d054505961d270fa768de150d598f8b7
BryanGF0822 <bryanicesi0822@gmail.com>
2021-08-30 15:12:05 -0500

    Se crea la tabla con las columnas respectivas.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,9 +30,13 @@ namespace taller_estadistica_lista_y_grafico
 
         private void btnAbrir_Click(object sender, RoutedEventArgs e)
         {
+            String fileSearch;
             OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.DefaultExt = ".csv";
+            openFileDialog.Filter = "Arichivos csv (.csv)|*.csv";
             if (openFileDialog.ShowDialog() == true)
             {
+                fileSearch = openFileDialog.FileName;
 
             }
                
@@ -45,6 +50,29 @@ namespace taller_estadistica_lista_y_grafico
         private void btnEjecutarFiltro_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void readerFile(String fileSearch)
+        {
+            string[] tokens;
+            char[] separators = { ';' };
+            string str = "";
+
+            FileStream fs = new FileStream(@"D:\Dokumenter\Skole\6. semester\GUI\Exercises\Exercise2\02 deltagerliste.csv",
+                                           FileMode.Open);
+            StreamReader sr = new StreamReader(fs, Encoding.Default);
+
+            while ((str = sr.ReadLine()) != null)
+            {
+                tokens = str.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+                Console.WriteLine(String.Format("{0,-20}", tokens[0]) +
+                                  String.Format("{0,-15}", tokens[1]) +
+                                  String.Format("{0,-15}", tokens[2]) +
+                                  String.Format("{0,-15}", tokens[3]));
+            }
+
+            Console.ReadLine();
         }
     }
 }
